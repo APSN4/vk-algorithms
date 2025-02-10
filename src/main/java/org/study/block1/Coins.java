229f7a2f97206d4c379714fd94a11d55f5ae314a
APSN4 <tema.sergeevich2012@yandex.ru>
2025-02-10 04:46:39 -0800

    feature: block1/Coins

--- src/main/java/org/study/block1/Coins.java
+++ src/main/java/org/study/block1/Coins.java
@@ -1,9 +1,9 @@
-package org.study;
+package org.study.block1;
 
-import java.util.*;
+import java.util.List;
 import java.util.stream.Collectors;
 
-public class Main {
+public class Coins {
     static int count = 0;
 
     public static void countCombinations(int target, List<Integer> coins, int index) {
@@ -40,18 +40,5 @@ public class Main {
             printCombinations(target - coin, coins, index, path, output);
             path.remove(path.size() - 1);
         }
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int target = scanner.nextInt();
-        List<Integer> coins = Arrays.asList(1, 5, 10);
-
-        countCombinations(target, coins, 0);
-        System.out.println(count);
-
-        StringBuilder output = new StringBuilder();
-        printCombinations(target, coins, 0, new ArrayList<>(), output);
-        System.out.print(output);
     }
 }
