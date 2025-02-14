630c88ed9eea810bc251ee12ef84aba32b159806
APSN4 <tema.sergeevich2012@yandex.ru>
2025-02-13 21:50:58 -0800

    feature: switch to the Go language & 3_arrays

---
+++ arrays/3_arrays.go
@@ -0,0 +1,26 @@
+package arrays
+
+import "fmt"
+
+func GetInput() {
+	var count int
+	fmt.Scanln(&count)
+
+	records := make([]int, count)
+	var value int
+	for i := 0; i < count; i++ {
+		fmt.Scan(&value)
+		records[i] = value
+	}
+	maxRecord(count, records)
+}
+
+func maxRecord(count int, records []int) {
+	maxValue := 0
+	for i := 0; i < count; i++ {
+		if records[i] > maxValue {
+			maxValue = records[i]
+		}
+	}
+	fmt.Println(maxValue)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module vk-algorithms
+
+go 1.23
